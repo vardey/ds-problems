6dd2ed2cd0f691287ec8bc78a492c46c4ffec724
vardey <43723614+vardey@users.noreply.github.com>
2021-11-30 22:02:57 +0530

    Create Find_the_repeating_and_the_missing.java

---
+++ Find_the_repeating_and_the_missing.java
@@ -0,0 +1,26 @@
+import java.util.Hashtable;
+public class MyClass {
+    public static void main(String args[]) {
+            int arr[]  = {4, 3, 6, 2, 1, 1};
+            int len = arr.length;
+        int sum = (len)*(len+1)/2;
+        
+        Hashtable<Integer,Integer> h = new Hashtable<Integer,Integer>();
+        
+        int duplicate = 0;
+        int arraySum = 0;
+        
+        for(int i=0;i<arr.length;i++){
+            if(!h.containsKey(arr[i])){
+                h.put(arr[i],i);
+                arraySum+=arr[i];
+            }
+            else{
+                duplicate = arr[i];
+            }
+            
+        }
+        
+        System.out.println("Duplicate, "+duplicate+ "sum" +sum + " array sum " + arraySum +" missing: "+(sum-arraySum));
+    }
+}
